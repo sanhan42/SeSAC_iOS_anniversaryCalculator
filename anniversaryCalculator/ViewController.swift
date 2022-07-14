c9259c8b6f6bc437aa4f6e36e3d733ce36c490a4
sanhan42 <sangmin613@gmail.com>
2022-07-14 18:41:56 +0900

    Finish?

--- anniversaryCalculator/ViewController.swift
+++ anniversaryCalculator/ViewController.swift
@@ -10,10 +10,66 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet var imageViewCollection: [UIImageView]!
+    @IBOutlet var dDayLabelList: [UILabel]!
+    @IBOutlet var dateLabelList: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setDatePickerStyle()
+        setImageViewStyle(list: imageViewCollection)
+        setDDayLabel(list: dDayLabelList)
+        setDateLabel(list: dateLabelList)
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dateChanged(_ sender: UIDatePicker) {
+        setDateLabel(list: dateLabelList)
+    }
+    
+    func setDateLabel(list:[UILabel]) {
+        for (index,label) in list.enumerated() {
+            label.textColor = .white
+            label.numberOfLines = 2
+            let format = DateFormatter()
+            format.locale = Locale(identifier: "ko_KR")
+            format.timeZone = TimeZone(abbreviation: "KST")
+            format.dateFormat = """
+            yyyy년
+            MM월 dd일
+            """
+            let date = Calendar.current.date(byAdding: .day, value: (index+1)*100, to: datePicker.date)!
+            label.text = format.string(from: date)
+            label.textAlignment = .center
+            
+            // 뒤 이미지 때문에 글자가 잘 안보여서 그림자를 넣어봄.
+            label.layer.shadowColor = UIColor.black.cgColor
+            label.layer.shadowRadius = 1
+            label.layer.shadowOpacity = 1.0
+            label.layer.shadowOffset = CGSize(width: 1, height: 1)
+            label.layer.masksToBounds = false
+        }
+    }
+    
+    func setDDayLabel(list:[UILabel]) {
+        for (index,label) in list.enumerated() {
+            label.text = "D+\(index+1)00"
+            label.textColor = .white
+            label.font = .boldSystemFont(ofSize: 18)
+            
+            // 뒤 이미지 때문에 글자가 잘 안보여서 그림자를 넣어봄.
+            label.layer.shadowColor = UIColor.black.cgColor
+            label.layer.shadowRadius = 1
+            label.layer.shadowOpacity = 1.0
+            label.layer.shadowOffset = CGSize(width: 1, height: 1)
+            label.layer.masksToBounds = false
+        }
+    }
+    
+    func setImageViewStyle(list:[UIImageView]){
+        for imageView in list {
+            imageView.layer.cornerRadius = 8
+        }
     }
     
     func setDatePickerStyle(){
